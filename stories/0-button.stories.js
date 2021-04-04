fb5ee6b21dc233ad4d21d8c1ea236c96772a30bd
alicorak <alicorak97@gmail.com>
2021-04-04 16:32:16 +0300

    initial

---
+++ stories/0-button.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from '..components/button.js';
+
+export default {
+  title: 'Button',
+ 
+}
+
+export const Button = () => {
+  return (
+    <Button>
+      😄😂📌
+    </Button>
+  );
+};
---
+++ stories/1-icons.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as Icons from '../components/icons'
+
+export default {
+  title: 'Icons'
+}
+
+export const Icon = () => {
+  return (
+    <Icons.ArrowBottom style={{color: "black"}}/>
+  );
+};
+
